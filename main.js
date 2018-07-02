989eca38a18092fdc26766888f5bc655d85b261d
Daniel Kang <danielkang674@gmail.com>
2018-07-02 14:23:36 -0500

    Refactor original code to MVC pattern

--- main.js
+++ main.js
@@ -1,15 +1,45 @@
 
 $(document).ready(function(){
-  var $body = $('body');
-  $body.html('');
 
-  var index = streams.home.length - 1;
-  while(index >= 0){
-    var tweet = streams.home[index];
-    var $tweet = $('<div></div>');
-    $tweet.text('@' + tweet.user + ': ' + tweet.message);
-    $tweet.appendTo($body);
-    index -= 1;
+  const model = {streams: streams, users: users};
+
+  const controller = {
+    init(){
+      view.init();
+    },
+    getAllTweets(){
+      return model.streams.home;
+    }
+  };
+
+  const view = {
+    init(){
+      this.$body = $('body');
+      this.allTweets = controller.getAllTweets();
+      this.$seeNewTweetsBtn = $('<button>See new tweets</button>');
+      this.$tweetArea = $('<div></div>');
+      this.render();
+    },
+    render(){
+      this.$body.html('');
+      this.seeNewTweets();
+      this.showAllTweets();
+    },
+    showAllTweets(){
+      this.$tweetArea.appendTo(this.$body);
+      this.allTweets.forEach((tweet)=>{
+        let $tweet = $('<div></div>');
+        $tweet.text(`@ ${tweet.user}: ${tweet.message}`);
+        this.$tweetArea.prepend($tweet);
+      });
+    },
+    seeNewTweets(){
+      this.$seeNewTweetsBtn.on("click", function(){
+        view.render();
+      });
+      this.$seeNewTweetsBtn.appendTo(this.$body);
+    }
   }
 
+  controller.init();
 });
